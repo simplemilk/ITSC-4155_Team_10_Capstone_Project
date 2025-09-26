99ff132236d62aa5db511f61a1d9aa23fb3b53dd
Mila Keener <milakeener@gmail.com>
2025-09-25 21:03:13 -0400

    updates

--- niner_finance/server.js
+++ niner_finance/server.js
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import financeRoutes from "./routes/financeRoutes.js";
 
 const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/finance", financeRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
